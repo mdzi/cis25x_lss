16514decf6204e594ccb853ede26de2856e0470e
Michael <m@mdzi.com>
2022-04-06 20:00:12 -0400

    spacing

--- NLB/configure.ps1
+++ NLB/configure.ps1
@@ -1,12 +1,10 @@
 $ComputerNames=("VM1","DC2")
-$ConfigPath="\\DC1\Software\IIS_NLB.xml"
 #Install IIS & NLB per configuration file
 $js = @()
 foreach($ComputerName in $ComputerNames) {
-$js += Start-Job -Command {
-Install-WindowsFeature "Web-Default-Doc","Web-Dir-Browsing","Web-Http-Errors","Web-Static-Content","Web-Http-Logging","Web-Stat-Compression","Web-Filtering","Web-Mgmt-Console","NLB","RSAT-NLB" -ComputerName
-$using:ComputerName -Restart
-}
+    $js += Start-Job -Command {
+        Install-WindowsFeature "Web-Default-Doc","Web-Dir-Browsing","Web-Http-Errors","Web-Static-Content","Web-Http-Logging","Web-Stat-Compression","Web-Filtering","Web-Mgmt-Console","NLB","RSAT-NLB" -ComputerName $using:ComputerName -Restart
+    }
 }
 Receive-Job -Job $js -Wait
 #Install RSAT NLB Configuration
@@ -23,28 +21,21 @@ $ClusterPorts = (80,443)
 $ClusterFrom = "255.255.255.255"
 #create a new cluster
 
-$cluster = (New-NLBCluster -HostName $primaryHost -InterfaceName $interface -
-ClusterName $ClusterAName -ClusterPrimaryIP $ClusterIP -SubnetMask $ClusterSN -
-OperationMode "Multicast")
+$cluster = (New-NLBCluster -HostName $primaryHost -InterfaceName $interface -ClusterName $ClusterAName -ClusterPrimaryIP $ClusterIP -SubnetMask $ClusterSN -OperationMode "Multicast")
 #Add a new node to the cluster
-$cluster | Add-NLBClusterNode -NewNodeName $secondaryHost -NewNodeInterface
-$interface
+$cluster | Add-NLBClusterNode -NewNodeName $secondaryHost -NewNodeInterface $interface
 #Add port rules to the cluster
 $cluster | Get-NLBClusterPortRule | Remove-NLBClusterPortRule -Force
-foreach ($Port in $ClusterPOrts) {
-$cluster | Add-NLBClusterPortRule -IP $ClusterFrom -Protocol TCP -StartPort
-$Port -EndPort $Port -Mode "Multiple" -Affinity "Single" -Timeout 0
+foreach ($Port in $ClusterPorts) {
+    $cluster | Add-NLBClusterPortRule -IP $ClusterFrom -Protocol TCP -StartPort $Port -EndPort $Port -Mode "Multiple" -Affinity "Single" -Timeout 0
 }
 #Create an identification file
 foreach($ComputerName in $ComputerNames) {
-Invoke-Command -ComputerName $ComputerName -ScriptBlock { $Env:ComputerName |
-Out-File C:\INETPUB\WWWROOT\default.htm -Encoding ASCII }
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock { $Env:ComputerName | Out-File C:\INETPUB\WWWROOT\default.htm -Encoding ASCII }
 }
 #Create DNS Records
-Add-DnsServerResourceRecordA -Name $ClusterBaseName -IPv4Address $ClusterIP -
-ZoneName $DNSZone
-Add-DnsServerResourceRecordCName -Name "www" -HostNameAlias $ClusterBaseName -
-ZoneName $DNSZone
+Add-DnsServerResourceRecordA -Name $ClusterBaseName -IPv4Address $ClusterIP -ZoneName $DNSZone
+Add-DnsServerResourceRecordCName -Name "www" -HostNameAlias $ClusterBaseName -ZoneName $DNSZone
 #both up
 (Invoke-WebRequest http://www.adatum.com).Content
 $cluster | Get-NlbClusterNode $primaryHost | Stop-NlbClusterNode
